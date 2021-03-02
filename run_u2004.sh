45b7033bd64910d1a05f5853c6fb1a86548afbb3
jstradle <jstrad@aol.com>
2021-03-02 09:28:35 -0500

    First Push

---
+++ run_u2004.sh
@@ -0,0 +1,1 @@
+docker run --rm -it ubuntu2004 /bin/bash
---
+++ tzdata.sh
@@ -0,0 +1,7 @@
+# set noninteractive installation
+export DEBIAN_FRONTEND=noninteractive
+#install tzdata package
+apt-get install -y tzdata
+# set your timezone
+ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
