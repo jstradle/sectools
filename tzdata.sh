38207172a6dcab4d4ba78abcf8d22fdc40f50f04
Jason Stradley <jstrad@aol.com>
2021-03-04 11:04:00 -0600

    Added travis.yml file to being testing using travisci. Configured cucumber.yml to reference "TARGET_HOST=localhost"

--- tzdata.sh
+++
@@ -1,7 +0,0 @@
-# set noninteractive installation
-export DEBIAN_FRONTEND=noninteractive
-#install tzdata package
-apt-get install -y tzdata
-# set your timezone
-ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
