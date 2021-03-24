59af0cf2ac8acc82d15abba0a6dda590a6df4de4
Jason Stradley <jstrad@aol.com>
2021-03-24 14:27:58 -0500

    added Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,11 @@
+pipeline {
+  agent any
+  stages {
+    stage('') {
+      steps {
+        git(url: 'https://github.com/jstradle/bash-test.git', branch: 'Origin', credentialsId: 'jstradle')
+      }
+    }
+
+  }
+}
--- run_sectools.sh
+++ run_sectools.sh
@@ -1,5 +1,4 @@
-docker run --name sectools --rm -td -v ~/secfiles:/gauntlt/bin/tmp jstrad/sectools:alpha /bin/bash
-#docker run --name sectools --rm -it -v ~/secfiles:/gauntlt/bin/tmp jstrad/sectools:alpha /bin/bash
+docker run --rm -it -v ~/secfiles:/gauntlt/bin/tmp jstrad/sectools:alpha /bin/bash
 #docker run --rm -it --name=sectools -v ~/secfiles:/opt/attack/ jstrad/sectools:alpha /bin/bash
 #docker run --rm -it --name=sectools sectools /bin/bash
 #docker run --rm --name=sectools sectools
